1431f93696d2a27c1a11f6020ba1e48ce030c01d
logrcia <lolagm04@gmail.com>
2023-11-02 16:38:42 -0300

    agregamos cables

--- js.js
+++ js.js
@@ -1,15 +1,16 @@
 var comenzar= document.getElementById("comenzar");
 var siguiente= document.getElementById("siguiente");
 var submit= document.getElementById("submit");
-var input= document.getElementById("input");
+var random = 0;
+var elegido = document.getElementsByClassName("cables");
+var tiempo = 90;
+var tiempoIncorrecto = 10;
 
-var cables = [
-    
-]
-
+var cables = ["amarillo", "azul", "gris", "lila", "marron", "naranja", "negro", "rojo", "rosa", "verde"];
 
 document.getElementById("todo").style.display = "none";
 document.getElementById("instrucciones").style.display = "none";
+document.getElementById("ganaste").style.display = "none";
 
 function onSubmit() {
     var input = document.getElementById("input");
@@ -23,9 +24,31 @@ siguiente.onclick= function(){
     document.getElementById("inicio").style.display = "none";
 }
 
+function randomColor(){
+    random = Math.floor(Math.random() * cables.length-1);
+    var colores= cables[random];
+    console.log(colores);
+}
+
 comenzar.onclick= function(){
     document.getElementById("instrucciones").style.display = "none";
     document.getElementById("todo").style.display = "block";
+    randomColor();
 }
 
+for (var i = 0; i < elegido.length; i++) {
+    elegido[i].onclick = function() {
+        var colorElegido = this.id; 
+        
+        if (colorElegido === cables[random]) {
+            document.getElementById("ganaste").style.display = "block";
+            document.getElementById ("todo").style.display = "none";
+        } else {
+            document.getElementById(this.id).style.display = "none";
+        }
+    }
+}
 
+setInterval(function timer (){
+    codigo!!
+}, 90);
