da519841e62742da20cc4175dd434a7367f12333
logrcia <lolagm04@gmail.com>
2023-12-04 00:07:21 -0300

    correcciones

--- js.js
+++ js.js
@@ -6,12 +6,19 @@ var elegido = document.getElementsByClassName("cables");
 var tiempoIncorrecto = 10;
 var timerInterval;
 var segundoNivel = document.getElementById("segundoNivel");
+var tercerNivel = document.getElementById("tercerNivel");
 var tiempo;
 var input = document.getElementById("input");
 var puntaje = 0;
 var nombre = document.getElementById("nombre");
 var imagenGanaste = document.getElementById("imagenGanaste");
-
+var todo = document.getElementById("todo");
+var instrucciones = document.getElementById("instrucciones");
+var ganaste = document.getElementById("ganaste");
+var modalPerdiste = document.getElementById("modalPerdiste");
+var inicio = document.getElementById("inicio");
+var niveles = document.getElementById("niveles");
+var score = document.getElementById("score");
 
 var cables = [
   "amarillo",
@@ -26,10 +33,10 @@ var cables = [
   "verde",
 ];
 
-document.getElementById("todo").style.display = "none";
-document.getElementById("instrucciones").style.display = "none";
-document.getElementById("ganaste").style.display = "none";
-document.getElementById("modalPerdiste").style.display = "none";
+todo.style.display = "none";
+instrucciones.style.display = "none";
+ganaste.style.display = "none";
+modalPerdiste.style.display = "none";
 
 
 
@@ -44,34 +51,31 @@ siguiente.onclick = function () {
   if (input.value === "") {
     alert("Por favor, introducí tu nombre antes de continuar.");
   } else {
-    document.getElementById("instrucciones").style.display = "block";
-    document.getElementById("inicio").style.display = "none";
+    instrucciones.style.display = "block";
+    inicio.style.display = "none";
   }
 };
 
 function randomColor() {
   random = Math.floor(Math.random() * cables.length);
   var colores = cables[random];
-  console.log(colores);
 }
 
 function iniciarTimer() {
   timerInterval = setInterval(function () {
     tiempo -= 1;
     document.getElementById("timer").innerText = tiempo;
-    console.log("Tiempo restante: " + tiempo + " segundos");
 
   if (tiempo <= 0) {
     clearInterval(timerInterval);
-    console.log("¡Tiempo agotado! La bomba explotó.");
     perdiste(); 
   }
   }, 1000);
 }
 
 function iniciar(timer) {
-  document.getElementById("modalPerdiste").style.display = "none";
-  document.getElementById("todo").style.display = "block";
+  modalPerdiste.style.display = "none";
+  todo.style.display = "block";
   document.getElementById("nombre").innerHTML = input.value;
   randomColor();
   iniciarTimer();
@@ -83,41 +87,33 @@ for (var i = 0; i < elegido.length; i++) {
       var colorElegido = this.id;
 
       if (colorElegido === cables[random]) {
-          document.getElementById("ganaste").style.display = "block";
-          document.getElementById("todo").style.display = "none";
+          ganaste.style.display = "block";
+          todo.style.display = "none";
           clearInterval(timerInterval);
           puntaje += 10;
-          document.getElementById("score").innerText = puntaje;
+          score.innerText = puntaje;
       } else {
           document.getElementById(this.id).style.display = "none";
           if (tiempo != 0) {
               tiempo -= tiempoIncorrecto;
               if (tiempo <= 0) {
-                  tiempo == 0;
+                  tiempo = 0;
                   perdiste();
               }
           } else {
               perdiste();
           }
-
-          console.log(
-              "Respuesta incorrecta. Se restan " +
-              tiempoIncorrecto +
-              " segundos. Tiempo restante: " +
-              tiempo +
-              " segundos"
-          );
       }
   };
 }
 
 comenzar.onclick = function () {
-  document.getElementById("instrucciones").style.display = "none";
+  instrucciones.style.display = "none";
   iniciar(90);
 };
 
 function proximoNivel() {
-  document.getElementById("ganaste").style.display = "none";
+  ganaste.style.display = "none";
 
   for (var i = 0; i < elegido.length; i++) {
     elegido[i].style.display = "none";
@@ -129,40 +125,37 @@ function proximoNivel() {
 }
 
 function perdiste() {
-  document.getElementById("modalPerdiste").style.display = "block";
-  document.getElementById("todo").style.display = "none";
+  modalPerdiste.style.display = "block";
+  todo.style.display = "none";
   puntaje = 0;
-  document.getElementById("score").innerText = puntaje;
+  score.innerText = puntaje;
   imagenGanaste.src = "./assets/Ganasteeeeeee.png";
 }
 
 function reiniciar() {
     iniciar(90);
     proximoNivel();
-    document.getElementById("niveles").innerHTML = "Nivel Fácil";
-    document.getElementById("tercerNivel").style.display = "none";
-    document.getElementById("segundoNivel").style.display = "block";
+    niveles.innerHTML = "Nivel Fácil";
+    tercerNivel.style.display = "none";
+    segundoNivel.style.display = "block";
 }
 
+tercerNivel.style.display = "none";
 
-document.getElementById("tercerNivel").style.display = "none";
+function cambiarNivel(tiempo, displaySegundoNivel, displayTercerNivel, nivelTexto, imagenGanasteSrc) {
+  niveles.innerHTML = nivelTexto;
+  iniciar(tiempo);
+  proximoNivel();
+  segundoNivel.style.display = displaySegundoNivel;
+  tercerNivel.style.display = displayTercerNivel;
+  score.innerText = puntaje;
+  imagenGanaste.src = imagenGanasteSrc;
+}
 
 segundoNivel.onclick = function () {
-  document.getElementById("niveles").innerHTML = "Nivel Medio";
-  iniciar(75);
-  proximoNivel();
-  document.getElementById("segundoNivel").style.display = "none";
-  document.getElementById("tercerNivel").style.display = "block";
-  document.getElementById("score").innerText = puntaje;
+  cambiarNivel(75, "none", "block", "Nivel Medio", "./assets/Ganasteeeeeee.png");
 };
 
 tercerNivel.onclick = function () {
-  document.getElementById("niveles").innerHTML = "Nivel Difícil";
-  iniciar(50);
-  proximoNivel();
-  document.getElementById("tercerNivel").style.display = "none";
-  document.getElementById("segundoNivel").style.display = "none";
-  document.getElementById("score").innerText = puntaje;
-  imagenGanaste.src = "./assets/GanasteFinal.png";
-}
-
+  cambiarNivel(50, "none", "none", "Nivel Difícil", "./assets/GanasteFinal.png");
+};
